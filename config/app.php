08c05bca51a56a774b6c565f87bdef2382601f05
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-04 20:31:26 +0300

    Tutor panel

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\Filament\TeacherPanelProvider::class,
+        App\Providers\Filament\TutorPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
